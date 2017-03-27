ed6ad21642a50301099614fb7fd2b2c5250e6ed6
Yin Chen <yinchen@freewheel.tv>
2017-03-27 10:57:29 +0800

    fix the formate and add the test file.

--- mock_service.go
+++ mock_service.go
@@ -4,10 +4,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-    "os"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 )
 
@@ -15,8 +15,8 @@ var ErrEndpointDoesNotExist = errors.New("Endpoint does not exist")
 var ErrCreatorNotExist = errors.New("Mocker Creator Endpoint does not exist.")
 
 type MockService struct {
-	mockCreatorEndpoint  string
-	mockedEndpoints      map[string]map[string]MockEndpoint
+	mockCreatorEndpoint string
+	mockedEndpoints     map[string]map[string]MockEndpoint
 	sync.Mutex
 }
 
@@ -36,8 +36,8 @@ type MockEndpointSeries []MockEndpoint
 
 func New(mockCreatorEndpoint string) *MockService {
 	return &MockService{
-		mockCreatorEndpoint:  mockCreatorEndpoint,
-		mockedEndpoints:      map[string]map[string]MockEndpoint{},
+		mockCreatorEndpoint: mockCreatorEndpoint,
+		mockedEndpoints:     map[string]map[string]MockEndpoint{},
 	}
 }
 
@@ -50,30 +50,32 @@ func (m *MockService) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	m.serveMockHTTP(w, req)
 }
 
-func (m *MockService) PreloadEndpointsFromConf(confFilePath string) (error, bool){
-    if  len(m.mockCreatorEndpoint) < 1 {
-        return  ErrCreatorNotExist, false
-    }
-    fi, err := os.Open(confFilePath)
-    if err != nil{panic(err)}
-    defer fi.Close()
-    fd, _ := ioutil.ReadAll(fi)
-    var series MockEndpointSeries
-    if err := json.Unmarshal(fd, &series); err != nil {
+func (m *MockService) PreloadEndpointsFromConf(confFilePath string) (error, bool) {
+	if "" == strings.Trim(m.mockRegistrationEndpoint) {
+		return ErrCreatorNotExist, false
+	}
+	fi, err := os.Open(confFilePath)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+	fd, _ := ioutil.ReadAll(fi)
+	var series MockEndpointSeries
+	if err := json.Unmarshal(fd, &series); err != nil {
 		log.Fatal("Unable to Unmarshal request body %s: %s", fd, err)
-        return err, false
-    }
-    for _, endpoint := range(series) {
-        m.CreateMockEndpoint(endpoint)
-    }
-    return nil, true
+		return err, false
+	}
+	for _, endpoint := range series {
+		m.CreateMockEndpoint(endpoint)
+	}
+	return nil, true
 }
 
 func (m *MockService) CreateMockEndpoint(endpoint MockEndpoint) {
-    if "POST" == endpoint.Method && m.mockCreatorEndpoint == endpoint.Endpoint {
+	if "POST" == endpoint.Method && m.mockCreatorEndpoint == endpoint.Endpoint {
 		log.Fatal("Ignore the overwriting for the mock creator endpoint.")
-        return
-    }
+		return
+	}
 	m.Lock()
 	if _, ok := m.mockedEndpoints[endpoint.Method]; !ok {
 		m.mockedEndpoints[endpoint.Method] = map[string]MockEndpoint{}
@@ -114,7 +116,6 @@ func (m *MockService) serveRegistrationHTTP(w http.ResponseWriter, req *http.Req
 	m.CreateMockEndpoint(endpointRequest)
 	w.WriteHeader(http.StatusCreated)
 }
-
 
 func (m *MockService) serveMockHTTP(w http.ResponseWriter, req *http.Request) {
 	endpoint, err := m.LookupEndpoint(req.Method, req.URL.Path)
